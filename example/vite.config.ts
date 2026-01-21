a01ab41180aecb8ec01d2639a6c514226dfdb1f9
halfmonty <mikehubley@gmail.com>
2026-01-21 15:29:59 -0600

    support github pages endpoint

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.CI ? "/naf/" : "/",
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
   },
 });
